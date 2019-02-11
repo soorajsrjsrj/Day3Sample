7f3cfcf0755b43ae69b2e33891b2f62a28691015
MacStudent <macstudent@126-iMac30.local>
2019-02-11 18:56:42 -0500

    Custom labelling

--- Day3Sample/main.swift
+++ Day3Sample/main.swift
@@ -8,26 +8,36 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Function")
+
+func hello(){
+    print("hello")
+}
+
+hello()
+
+func add(a:Int ,b:Int){
+    let c = a+b;
+    print("sum of Int \(c)")
+}
 
 
 
+add(a: 5, b: 6)
 
+func add(a:Float,b:Float){
+    let c = a+b;
+    print("sum of Float \(c)")
+}
 
+add(a: 2.00, b: 3.45)
 
+func sum(first a:Int, andsecond b:Int){
+    let c = a+b;
+    print("sum of labelled funtion we create our own custom label \(c)")                            //default label
+}
 
-print("sapce")
-print("space")
-
-
-
-
-
-
-
-
-
-
+sum(first: 20, andsecond: 30)
 
 
 
